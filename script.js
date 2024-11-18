813a85acc398a0fa683a17313f066ad38f2688ba
wholstenholmef <newlight.newmachinee@gmail.com>
2024-11-18 11:25:01 -0500

    actualizacion clase 18/11/24

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    var menu = document.querySelector('.menu');
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+document.addEventListener('DOMContentLoaded', () => {
+    updateContentVisibility();
 });
+
+function updateContentVisibility() {
+    const isLogged = localStorage.getItem('logeado') === 'true';
+    /*document.getElementById('loginBox').style.display = isLogged ? 'none' : 'block';*/
+    document.getElementById('userContent').style.display = isLogged ? 'block' : 'none';
+}
+
+function login() {
+    localStorage.setItem('logeado', 'true');
+    updateContentVisibility();
+}
+
+function close_session() {
+    localStorage.setItem('logeado', 'false');
+    updateContentVisibility();
+}
