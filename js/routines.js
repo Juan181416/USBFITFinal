4e8b1af67792fa1afdb26afc656a1c1ca63103f2
Juan181416 <juan181416@gmail.com>
2024-11-23 22:25:50 -0500

    mas cositas a las paginas

--- js/routines.js
+++
@@ -1,9 +0,0 @@
-const myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
-// or
-const myModalAlternative = new bootstrap.Modal('#myModal', options)
-
-
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
